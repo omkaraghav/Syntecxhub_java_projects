9028cc7159032ad3334fa1f112ed21c672b68348
omkaraghav <97125738+omkaraghav@users.noreply.github.com>
2026-07-11 14:21:53 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,7 +1,40 @@
 public class Employee {
-    private int id; private String name; private String department; private double salary;
-    public Employee(int id,String name,String department,double salary){this.id=id;this.name=name;this.department=department;this.salary=salary;}
-    public int getId(){return id;} public String getName(){return name;} public String getDepartment(){return department;} public double getSalary(){return salary;}
-    public void setName(String n){name=n;} public void setDepartment(String d){department=d;} public void setSalary(double s){salary=s;}
-    public String toString(){return "\nEmployee ID : "+id+"\nName : "+name+"\nDepartment : "+department+"\nSalary : ₹"+salary+"\n-----------------------------";}
+    private int id; 
+    private String name; 
+    private String department;
+    private double salary;
+    public Employee(int id,String name,String department,double salary){
+        this.id=id;this.name=name;this.department=department;this.salary=salary;
+    }
+    public int getId(){
+        return id;
+    } 
+    public String getName()
+    {
+        return name;
+    }
+    public String getDepartment()
+    {
+        return department;
+    } 
+    public double getSalary()
+    {
+        return salary;
+    }
+    public void setName(String n)
+    {
+        name=n;
+    } 
+    public void setDepartment(String d)
+    {
+        department=d;
+    }
+    public void setSalary(double s)
+    {
+        salary=s;
+    }
+    public String toString()
+    {
+        return "\nEmployee ID : "+id+"\nName : "+name+"\nDepartment : "+department+"\nSalary : ₹"+salary+"\n-----------------------------";
+    }
 }
